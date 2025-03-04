2153f404d375699a33c0a8de0fa390c9e20e5757
BenjaminDanker <silvergem95@gmail.com>
2025-03-04 12:26:36 -0600

    Fix image request occurring three times

--- obj/Debug/net7.0/TodoWeatherApp.AssemblyInfo.fs
+++ obj/Debug/net7.0/TodoWeatherApp.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("TodoWeatherApp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe5d00a81732e0470c11b5a1b4a8da0a1fb89a8")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f348218c8a960df45a24d16c1f52762b7904c3c")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("TodoWeatherApp")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("TodoWeatherApp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
